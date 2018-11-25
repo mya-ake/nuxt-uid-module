62758ea36647cec06b1700df8276cf5c04b3dde8
mya-ake <make.app.ts@gmail.com>
2018-11-25 19:24:08 +0900

    feat: Update for mixin #1

--- src/module.js
+++ src/module.js
@@ -1,14 +1,33 @@
 import path from 'path';
 
+const defaultOptions = {
+  plugin: true,
+};
+
 export default function nuxtUidModule(_moduleOption) {
   const moduleOption = {
+    ...defaultOptions,
     ...this.options['uid-module'],
     ..._moduleOption,
   };
 
+  const options = {};
+  // use plugin option
+  if (moduleOption.plugin) {
+    options.usePlugin = true;
+  }
+
+  // use name option
+  if (typeof moduleOption.name === 'string') {
+    options.name = moduleOption.name;
+  }
+
+  delete moduleOption.plugin;
+  options.plugin = JSON.stringify(moduleOption);
+
   this.addPlugin({
     src: path.resolve(__dirname, 'plugin.template.js'),
     ssr: true,
-    options: JSON.stringify(moduleOption),
+    options,
   });
 }
--- src/plugin.template.js
+++ src/plugin.template.js
@@ -1,9 +1,17 @@
 import Vue from 'vue';
 import VueUid, { uid } from 'vue-uid';
 
-Vue.use(VueUid, {
-  ...JSON.parse('<%= options %>'),
+<% if (options.usePlugin) { %>
+
+  Vue.use(VueUid, {
+  ...JSON.parse('<%= options.plugin %>'),
 });
+
+<% } else if (options.name) { %>
+
+uid.setName('<%= options.name %>')
+
+<% } %>
 
 export default () => {
   uid.reset();
